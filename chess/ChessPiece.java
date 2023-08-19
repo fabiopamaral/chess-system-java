4da6a77f46b97dc3123d14fc034cfbde2b9e873c
fabiopamaral <fabio.pamaral@outlook.com>
2023-08-19 00:18:54 -0300

    Check logic

--- chess/ChessPiece.java
+++ chess/ChessPiece.java
@@ -5,20 +5,24 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-    
-    private Color color;
 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
+	private Color color;
 
-    public Color getColor() {
-        return color;
-    }
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
 
-    protected boolean isThereOpponentPiece(Position position) {
-        ChessPiece p = (ChessPiece)getBoard().piece(position);
-        return p != null && p.getColor() != color;
-    }
+	public Color getColor() {
+		return color;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
